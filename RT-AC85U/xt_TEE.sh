05d064a96ebdccd25df9132f684c83bab93791de
Youhei Sakurai <sakurai.youhei@gmail.com>
2020-06-05 17:25:47 +0900

    Small improvements

--- RT-AC85U/xt_TEE.sh
+++ RT-AC85U/xt_TEE.sh
@@ -2,11 +2,14 @@
 
 if [ $# -ne 1 ]; then
   echo "Usage:" 1>&2
-  echo "  wget -q -O - http://website.ferveyes.com/RT-AC85U/xt_TEE.sh | sh IP_OF_TEE_GATEWAY" 1>&2
+  echo "  wget -q -O - http://website.ferveyes.com/RT-AC85U/xt_TEE.sh | sh -s IP_OF_TEE_GATEWAY" 1>&2
   exit 1
 fi
 
-wget -O /tmp/xt_TEE.ko http://website.ferveyes.com/RT-AC85U/xt_TEE.ko
+wget -q -O /tmp/xt_TEE.ko http://website.ferveyes.com/RT-AC85U/xt_TEE.ko
+
+iptables -t mangle --flush
+rmmod xt_TEE.ko
 insmod /tmp/xt_TEE.ko
 
 iptables -t mangle -I PREROUTING -i br0 -j TEE --gateway "$1"
