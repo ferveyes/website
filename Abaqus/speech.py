3b455fd21491f9f21ad8ff3a256bfbc29f921653
Youhei Sakurai <sakurai.youhei@gmail.com>
2020-06-28 19:54:34 +0900

    ja-JP

--- Abaqus/speech.py
+++ Abaqus/speech.py
@@ -1,8 +1,10 @@
 from browser import window
 
 
-def say(text, *args, onend=None):
+def say(text, *args, onend=None, lang=None):
     ut = window.SpeechSynthesisUtterance.new(str(text).format(*args));
     if callable(onend):
         ut.onend = onend
+    if lang:
+        tu.lang = lang
     window.speechSynthesis.speak(ut);
